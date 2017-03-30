6598c0c235600095aeab831cdc014a15b062b5c3
Nicolay <Nicolay.JG@outlook.com>
2017-03-30 08:42:19 -0500

    Hobby on the GO V0.02! Implementación de nuevas funciones

---
+++ Pagina web v0.02/fuente/Persistence/Conexion.php
@@ -0,0 +1,45 @@
+<?php
+	include 'dataDB.php';
+	
+	class conexion{	
+
+		function guardar($desc, $date1){   
+			$dataDB = new dataDB;	
+			$connection= new mysqli($dataDB->getHost(), $dataDB->getUsername(), $dataDB->getPass(), $dataDB->getDB()) or die ("Error al Conecatar con la base de datos");			
+			$query="INSERT INTO EVENT (descr,fecha,usuario) VALUES ('$desc','$date1',1);";
+			$rs= $connection->query($query);
+		}
+		
+		function cargarGF($fecha_inicio, $genero){
+				
+			$fecha= date("Y-m-d", strtotime("$fecha_inicio"));
+			//echo $fecha."<br>";
+
+			
+			$dataDB = new dataDB();	
+			$connection= new mysqli($dataDB->getHost(), $dataDB->getUsername(), $dataDB->getPass(), $dataDB->getDB()) or die ("Error al Conecatar con la base de datos");
+			$query ="select * from EVENTS where genero ='$genero' AND fecha='$fecha'";
+			$rs= $connection->query($query);
+			
+			return $rs;
+		}	
+
+		function cargarGenero($genero){
+				
+			
+			//echo $genero;
+
+			/*echo $date->format('Y-m-d');*/
+			/*echo $_PO0ST['date2'];*/
+			//realiza consulta1
+			
+			
+			$dataDB = new dataDB();	
+			$connection= new mysqli($dataDB->getHost(), $dataDB->getUsername(), $dataDB->getPass(), $dataDB->getDB()) or die ("Error al Conecatar con la base de datos");
+			$query ="select * from EVENTS where genero ='$genero'";
+			$rs= $connection->query($query);
+			
+			return $rs;
+		}
+	}
+?>
--- Pagina web v0.02/fuente/Persistence/cargar.php
+++ Pagina web v0.02/fuente/Persistence/cargar.php
@@ -28,8 +28,9 @@
 				<!-- Intro -->
 					<section id="intro" class="wrapper style1 fullscreen fade-up">
 						<div class="inner">
-							<h1>Eventos al Aire Libre</h1>
-							<!--
+							<h1>Resultados de la consulta</h1>
+							<p>Eventos para la fecha seleccionada </p>
+
 							<?php
 							include 'conexion.php';
 								
@@ -55,35 +56,16 @@
 										/*echo "llegue al while";*//*
 										while($row=$data->fetch_assoc()){
 										/*	echo "entre al while";*//*
-											echo '<br>'.$row['descr'].' / fecha: '.$row['fecha'] .'<br>';
+											echo '<br>'.$row['descr'].' / fecha: '.	$row['fecha'] .'<br>';
 
 										}
 									}*/	
 								}
 
 							?>
-							-->
-							<br>
-							
-							<label for="">Listar por fecha: </label>
-
-							<form action="fuente/Persistence/cargar.php" method= post name= "cargar">	
-											<div class="field">
-													<label for="date2">Fecha: </label>
-													<input name= "date2" id="date2" type="date" min = "2017-03-17" value = "2017-03-17"></input>
-													
-											</div>
-										
-										<ul class="actions">
-											<li><a href="" class="button submit" >Buscar</a></li>
-										</ul>
-
 							<ul class="actions">
-								<li><a href="index.html" class="button scrolly">Regresar</a></li>
+								<li><a href="../../index.html" class="button scrolly">Regresar</a></li>
 							</ul>
-							
-							</form>
-
 						</div>
 					</section>
 
