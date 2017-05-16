6b1eb23cd3253fffd203afa75c16915b22e6c925
Alexander <alexanderc@outlook.com>
2017-05-16 12:04:05 -0500

    Prueba cambio con errores ultimo

--- tests/PUConexion.php
+++ tests/PUConexion.php
@@ -2,9 +2,9 @@
 use PHPUnit\Framework\TestCase;
 include_once 'DataDb.php';
 class PUConexion extends TestCase{
-  
+
 	    public function testHost() {
-	    
+
 		    $d = new DataDb();
 		    /*$expect = 'localhost';*/
 		    $expect = 'sql202.rf.gd';
@@ -12,28 +12,28 @@ class PUConexion extends TestCase{
 	        $this->assertEquals($expect, $actual);
 		}
 	  	public function testUsername() {
-	    
+
 	        $data = new DataDB();
 	        /*$expected = "root";*/
 	        $expected = "rfgd_19756503";
 	        $actual = $data->getUsername();
 	        $this->assertEquals($expected, $actual);
 	    }
-	  
+
 	   public function testDB() {
-	    
+
 	        $data = new DataDB();
 	        /*$expected = "hobby";*/
 	        $expected = "rfgd_19756503_hobbyonthegoDB";
 	        $actual = $data->getDB();
 	        $this->assertEquals($expected, $actual);
 	    }
-	  
+
 	   	public function testPass() {
-	    
+
 	        $data = new DataDB();
 	        /*$expected = "";*/
-	        $expected = "mtd3pbWo";
+	        $expected = "mtd3pbW";
 	        $actual = $data->getPass();
 	        $this->assertEquals($expected, $actual);
 	    }
