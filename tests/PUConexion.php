8fe035e5408445f3cffbee780258bc94c048e5a8
Nicolay <Nicolay.JG@outlook.com>
2017-05-15 21:39:24 -0500

    Correccion "Namespace" clase PUConexion.php

--- tests/PUConexion.php
+++ tests/PUConexion.php
@@ -1,16 +1,12 @@
 <?php
-
-namespace Test;
-/*require_once 'vendor/Autoload.php';*/
 use PHPUnit\Framework\TestCase;
 include_once 'DataDb.php';
-
 class PUConexion extends TestCase{
-	  	
-		public function testHost() {
-	   
-	  	$d = new DataDb();
-	  	$expect = 'localhost';
+  
+    public function testHost() {
+    
+	    $d = new DataDb();
+	    $expect = 'localhost';
         $actual = $d->getHost();
         $this->assertEquals($expect, $actual);
 	    }
